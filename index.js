2b1d970a43fcceb0f6c9ce162ef0d8cf106fa15b
dudahcardoso <83031145+dudahcardoso@users.noreply.github.com>
2021-08-02 17:15:00 +0000

    Subindo OK

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-sleep(10)
 console.log('Olá, aconteceu um crime nessa cidade, a Carla foi assassinada brutalmente, eu estou encarregado de investigar esse crime, por isso preciso te fazer algumas perguntas... Vamos lá!')
-sleep(5)
-pergunta1 = prompt('Você telefonou para a vítima? [S/N] ').strip().upper()[0])
-pergunta2 = prompt('Você esteve no local do crime? [S/N] ').strip().upper()[0])
-pergunta3 = prompt('Você mora perto da vítima? [S/N] ').strip().upper()[0])
-pergunta4 = prompt('Você devia para a vítima? [S/N] ').strip().upper()[0])
-pergunta5 = prompt('Você já trabalhou com a vítima? [S/N] ').strip().upper()[0])
-cont = 0
 
-if(pergunta1 == 'S')
-   cont += 1
-if(pergunta2 == 'S')
-   cont += 1
-if(pergunta3 == 'S')
-   cont += 1
-if(pergunta4 == 'S')
-   cont += 1
-if(pergunta5 == 'S')
-   cont += 1
+let pergunta1 = prompt('Você telefonou para a vítima? [S/N] ')
+let pergunta2 = prompt('Você esteve no local do crime? [S/N] ')
+let pergunta3 = prompt('Você mora perto da vítima? [S/N] ')
+let pergunta4 = prompt('Você devia para a vítima? [S/N] ')
+let pergunta5 = prompt('Você já trabalhou com a vítima? [S/N] ')
 
-if(cont == 2)
-   print('Você é suspeito do crime!')
-elif 3 <= cont <= 4:
-   print('Você é cúmplice do crime!')
-elif cont == 5:
-   print('Você é o assassino!')
-else: 
-   print('Você é inocente de todas as acusações!')
+let cont = 0
+
+if(pergunta1 == 'S' || pergunta1 == 's')
+   cont ++
+   //console.log(cont)
+if(pergunta2 == 'S' || pergunta2 == 's')
+   cont ++
+   //console.log(cont)
+if(pergunta3 == 'S' || pergunta3 == 's')
+   cont ++
+   //console.log(cont)
+if(pergunta4 == 'S' || pergunta4 == 's')
+   cont ++
+   //console.log(cont)
+if(pergunta5 == 'S' || pergunta5 == 's')
+   cont ++
+   //console.log(cont)
+
+console.log(cont)
+
+
+if(cont == 2){
+  console.log('Você é suspeito do crime!')
+  }
+else if(cont == 3 || cont == 4){
+  console.log('Você é cúmplice do crime!')
+  }
+else if(cont == 5){  
+  console.log('Você é o assassino!')
+  }
+else{
+  console.log('Você é inocente de todas as acusações!')
+  }
